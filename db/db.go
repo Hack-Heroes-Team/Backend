9ede87b2ec4c4cdebcb007f02e7bd668ba465e9e
Mikolaj <mikolaj.bubacz@gmail.com>
2022-10-21 20:23:52 +0200

    ANTI PATTERN, DB CREDENTIALS IN REPO FOR NOW, CHANGE AS FAST AS POSSIBLE

--- db/db.go
+++ db/db.go
@@ -9,7 +9,7 @@ import (
 )
 
 func Init() *gorm.DB {
-	db, err := gorm.Open(postgres.Open("postgres://postgres:pass@localhost:5432/hack"), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open("postgres://daamrfehofxpau:1dd396b925c7b2655ec3dc8abb72a016cc1f9bf3e38260b637f89ba6b8635e74@ec2-34-248-169-69.eu-west-1.compute.amazonaws.com:5432/delp14k9rbbup"), &gorm.Config{})
 	sqlDB, _ := db.DB()
 	sqlDB.SetMaxOpenConns(10000000)
 	sqlDB.SetConnMaxLifetime(10 * time.Second)
