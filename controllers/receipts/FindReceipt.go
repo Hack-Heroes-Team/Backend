eaea76ed4f94d0ecb0785ff52349efdde021ec29
Mikolaj <mikolaj.bubacz@gmail.com>
2022-10-23 16:30:20 +0200

    fast fixes

---
+++ controllers/receipts/FindReceipt.go
@@ -0,0 +1,39 @@
+package receipts
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/mic3b/hack-backend/db"
+	"github.com/mic3b/hack-backend/models"
+)
+
+func FindReceipt(c *gin.Context) {
+	DB := db.Init()
+
+	var userMail models.Receipt
+	var receipts []models.Receipt
+
+	if err := c.ShouldBindJSON(&userMail); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	fmt.Println(userMail)
+
+	DB.Table("receipts").Where("id = ?", userMail.Id).Find(&receipts)
+
+	for i, v := range receipts {
+		var items []models.Item
+		DB.Table("items").Where("receipt_id = ?", v.Id).Find(&items)
+		receipts[i].Items = items
+		var sum float64
+		for _, v := range items {
+			sum = sum + v.Price
+		}
+		receipts[i].Price = sum
+	}
+
+	c.JSON(http.StatusOK, gin.H{"receipt": receipts[0]})
+}
