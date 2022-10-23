eb6b4d80a59a038a7a6975b2fc4d89860e6fd325
Mikolaj <mikolaj.bubacz@gmail.com>
2022-10-23 23:41:22 +0200

    last fixes

--- db/db.go
+++ db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"log"
+	"os"
 	"time"
 
 	"gorm.io/driver/postgres"
@@ -9,7 +10,8 @@ import (
 )
 
 func Init() *gorm.DB {
-	db, err := gorm.Open(postgres.Open("postgres://daamrfehofxpau:1dd396b925c7b2655ec3dc8abb72a016cc1f9bf3e38260b637f89ba6b8635e74@ec2-34-248-169-69.eu-west-1.compute.amazonaws.com:5432/delp14k9rbbup"), &gorm.Config{})
+	port := os.Getenv("DB_CRED")
+	db, err := gorm.Open(postgres.Open(port), &gorm.Config{})
 	sqlDB, _ := db.DB()
 	sqlDB.SetMaxOpenConns(10000000)
 	sqlDB.SetConnMaxLifetime(10 * time.Second)
